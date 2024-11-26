f1a978971ceba14654cd1e80a2720a78ab208ca9
IsuruAriyarathne <iahewababarand@astral.sciclone.wm.edu>
2024-11-26 11:20:56 -0500

    Add new rnn classifiers

--- classifiers/run_job_classifier.csh
+++ classifiers/run_job_classifier.csh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -J bloc-cnn-classifier       # Job name
-#SBATCH --output=/sciclone/data10/iahewababarand/bloc-cnn/logs/bloc-cnn-classifier-output.log        # Output file
-#SBATCH --error=/sciclone/data10/iahewababarand/bloc-cnn/logs/bloc-cnn-classifier-error.log       # Error file
+#SBATCH --output=/sciclone/data10/iahewababarand/Deep_Learning_Project/classifiers/logs/bloc-cnn-classifier-output.log        # Output file
+#SBATCH --error=/sciclone/data10/iahewababarand/Deep_Learning_Project/classifiers/logs/bloc-cnn-classifier-error.log       # Error file
 #SBATCH -N 1                   # Number of nodes
 #SBATCH -n 1                   # Number of tasks/cores per node
 #SBATCH --gres=gpu:1           # Request 1 GPU
 #SBATCH -t 24:00:00              # Runtime in hh:mm:ss
 #SBATCH --mem=20G
 
-python classifiers/run_notebook.py > ./logs/outputs.txt
+python run_notebook.py > ./logs/outputs.txt
--- classifiers/run_notebook.py
+++ classifiers/run_notebook.py
@@ -1,7 +1,19 @@
 import papermill as pm
 
-# Execute the notebook and save the output to a new file
+# both action, content
 pm.execute_notebook(
-    '.rnn/rnn.ipynb',  # Path to the input notebook
-    './rnn/output_notebook_v1.ipynb'        # Path to the output notebook
+    './rnn/only_action_method/rnn.ipynb',  # Path to the input notebook
+    './rnn/only_action_method/notebooks/output_notebook_v5(100 epoch).ipynb'        # Path to the output notebook
 )
+
+# #  action
+# pm.execute_notebook(
+#     './rnn/only_action_method/rnn-action.ipynb',  # Path to the input notebook
+#     './rnn/only_action_method/notebooks/output_notebook_v3(100 epoch).ipynb'        # Path to the output notebook
+# )
+
+# #  content
+# pm.execute_notebook(
+#     './rnn/only_action_method/rnn-content.ipynb',  # Path to the input notebook
+#     './rnn/only_action_method/notebooks/output_notebook_v4(100 epoch).ipynb'        # Path to the output notebook
+# )
